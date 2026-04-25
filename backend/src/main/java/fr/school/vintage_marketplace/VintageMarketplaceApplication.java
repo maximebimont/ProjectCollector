fd20becade940d49df082de895a1f1a6ec236839
Maxime Bimont <mbperso2311@gmail.com>
2026-04-25 20:43:22 +0200

    init spring

---
+++ backend/src/main/java/fr/school/vintage_marketplace/VintageMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package fr.school.vintage_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintageMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintageMarketplaceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fr/school/vintage_marketplace/VintageMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.school.vintage_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintageMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
